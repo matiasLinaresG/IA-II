8ed7a080d6b19385c6db990600ada22fbabe2d7c
Tomasmerca <32470264+Tomasmerca@users.noreply.github.com>
2023-04-13 00:06:14 -0300

    Super Ultra Optimizacion
    Cambia la funcion Calidad y la forma en que la llama el AlgGenetico

--- TP1/AlgoritmoGenetico.py
+++ TP1/AlgoritmoGenetico.py
@@ -2,113 +2,142 @@ from Funciones_Genetico import *
 import numpy as np
 import pandas as pd
 import random
+import time
+import multiprocessing as mt
 
 
-N_poblacionTotal=12
-N_poblacionInter=7
-iter_max=1000
-
-IdEstantes=pd.read_csv('ID_estantes.csv')
-IdEstantes=IdEstantes["ID"].values.tolist()
-
-IdProductos=pd.read_csv('productos.csv')
-IdProductos=IdProductos["id"].values.tolist()
-
-#leer el archivo order1.txt y guardar su informacoin en una lista de listas. esta separado po 'n y ,
-with open('ordenes_ordenadas.txt', 'r') as file:
-    contenido = file.read()
-ordenes = contenido.split('\n')
-for i in range(len(ordenes)):
-    ordenes[i]=ordenes[i].split(',')
+def calculate(func, args):
+    result = func(*args)
+    return result
 
 
-#igualar la canditad de productos y la cantidad de estantes rellendo prucuntos  con "vacio"
-for i in range(len(IdEstantes)-len(IdProductos)):
-  IdProductos.append("vacio")
+def calculatestar(args):
+    return calculate(*args)
 
 
-Poblacion=[]
-Poblacion_anterior=True
-if Poblacion_anterior:
-    #levantar la poblacion del archivo txt "poblacion"
-    with open('poblacion.txt', 'r') as file:
+if __name__ == '__main__':
+
+    N_poblacionTotal = 12
+    N_poblacionInter = 7
+    iter_max = 100
+
+    IdEstantes = pd.read_csv('ID_estantes.csv')
+    IdEstantes = IdEstantes["ID"].values.tolist()
+
+    IdProductos = pd.read_csv('productos.csv')
+    IdProductos = IdProductos["id"].values.tolist()
+
+    # leer el archivo order1.txt y guardar su informacoin en una lista de listas. esta separado po 'n y ,
+    with open('ordenes_ordenadas.txt', 'r') as file:
         contenido = file.read()
+    ordenes = contenido.split('\n')
+    for i in range(len(ordenes)):
+        ordenes[i] = ordenes[i].split(',')
 
-    Poblacion = contenido.split('\n')
-    for i in range(len(Poblacion)):
-        Poblacion[i]=Poblacion[i].split(',')
+    # igualar la canditad de productos y la cantidad de estantes rellendo prucuntos  con "vacio"
+    for i in range(len(IdEstantes)-len(IdProductos)):
+        IdProductos.append("vacio")
 
+    Poblacion = []
+    Poblacion_anterior = False
+    if Poblacion_anterior:
+        # levantar la poblacion del archivo txt "poblacion"
+        with open('poblacion.txt', 'r') as file:
+            contenido = file.read()
 
-    print(Poblacion[1])
-else:
-    for i in range(N_poblacionTotal):
-        Poblacion.append(np.random.permutation(IdProductos).tolist()) 
-
-fijos=round(N_poblacionInter*0.4)
-nofijos=N_poblacionTotal-fijos
-
-if nofijos%2!=0:
-    nofijos-=1
-    fijos+=1
-
-
-
-for k in range(iter_max):
-    print("ITERACION NUMERO: ",k)
-#CALIDAD. La idea es no calcular otra vez la calidad de los fijos
-    if k==0:
-        CalidadP=Calidad(Poblacion,ordenes,IdEstantes)
-    else:
-        for c in CalidadP:
-           print("\tCalidad ind(fijo)",c)
-        CalidadP=CalidadP+Calidad(Poblacion[fijos:],ordenes,IdEstantes)
-
-#SELECCION
-    CalidadPInter,PoblacionInter=SelecionarPoblacion(Poblacion,CalidadP,N_poblacionInter)
-#CONVERGENCIA
-    #if Convergencia(CalidadPInter,N_poblacionInter-5):
-    #    break
-
-    print("Calidad minimina individuo: ",min(CalidadP))
-#GENERACION DE UNA NUEVA POBLACION  <<========
-    Poblacion=[]
-    CalidadP=[]
-#FIJOS
-
-    Poblacion=PoblacionInter[:fijos]
-    CalidadP=CalidadPInter[:fijos]
-#CRUCE Y MUTACION    
-    for i in range(int(nofijos/2)):
-    
-        Padres=random.sample(PoblacionInter,2)
-        hijo1,hijo2=Cruce(Padres[0],Padres[1])
-        hijo1M=Mutacion(hijo1,6)
-        hijo2M=Mutacion(hijo2,6)
-        Poblacion.append(hijo1M)
-        Poblacion.append(hijo2M)
-
-    #guardar la poblacion  en un archivo csv usando
-    with open('poblacion.txt', 'w') as file:
+        Poblacion = contenido.split('\n')
         for i in range(len(Poblacion)):
-            for j in range(len(Poblacion[i])):
-                file.write(str(Poblacion[i][j]))
-                if j < len(Poblacion[i])-1:
-                    file.write(",")
-            if i < len(Poblacion)-1:
-                file.write("\n")
+            Poblacion[i] = Poblacion[i].split(',')
 
+        # print(Poblacion[1])
+    else:
+        for i in range(N_poblacionTotal):
+            Poblacion.append(np.random.permutation(IdProductos).tolist())
 
+    fijos = round(N_poblacionInter*0.4)
+    nofijos = N_poblacionTotal-fijos
 
+    if nofijos % 2 != 0:
+        nofijos -= 1
+        fijos += 1
 
+    for k in range(iter_max):
+        print("ITERACION NUMERO: ", k)
+    # CALIDAD. La idea es no calcular otra vez la calidad de los fijos
+        if k == 0:
+            # Pruebo hacerlo en un Pool
+            t0 = time.time()
+            with mt.Pool() as pool:
+                tareas = [(Calidad_individual, (indiv, ordenes, IdEstantes))
+                          for indiv in Poblacion]
+                try:
+                    CalidadP = pool.map(calculatestar, tareas)
+                except mt.TimeoutError:
+                    print("Se acabo el tiempo")
+                    
+            print("Tarde en calcular: (s) "+str(time.time()-t0))
+            # t0 = time.time()
+            # CalidadP2 = Calidad(Poblacion, ordenes, IdEstantes)
+            # print("A la antigua tardaba en calcular todo: (s) "+str(time.time()-t0))
+            
+        else:
+            t0 = time.time()
+            with mt.Pool() as pool:
+                tareas = [(Calidad_individual, (indiv, ordenes, IdEstantes))
+                          for indiv in Poblacion[fijos:]]
+                try:
+                    CalidadNOfijos = pool.map(calculatestar, tareas)
+                except mt.TimeoutError:
+                    print("Se acabo el tiempo")
+            CalidadP += CalidadNOfijos
+            print("Tarde en calcular: (s) "+str(time.time()-t0))
+            # t0 = time.time()
+            # for c in CalidadP:
+            #     print("\tCalidad ind(fijo)", c)
+            # CalidadP = CalidadP+Calidad(Poblacion[fijos:], ordenes, IdEstantes)
+            # print("A la antigua tardaba en calcular todo: (s) "+str(time.time()-t0))
 
+    # SELECCION
+        CalidadPInter, PoblacionInter = SelecionarPoblacion(
+            Poblacion, CalidadP, N_poblacionInter)
+    # CONVERGENCIA
+        # if Convergencia(CalidadPInter,N_poblacionInter-5):
+        #    break
 
+        print("Calidad minimina individuo: ", min(CalidadP))
+    # GENERACION DE UNA NUEVA POBLACION  <<========
+        Poblacion = []
+        CalidadP = []
+    # FIJOS
 
-print("==============RESPUESTA===============")
-solucion=Poblacion[list(CalidadP).index(min(CalidadP))]
+        Poblacion = PoblacionInter[:fijos]
+        CalidadP = CalidadPInter[:fijos]
+    # CRUCE Y MUTACION
+        for i in range(int(nofijos/2)):
 
-print("Número de iteraciones: "+str(k))
-print("Distancia mas corta: "+str(min(CalidadP)))
-print("Solución: "+str(solucion))
-print("Última poblacion intermedia: ")
-for i in range(len(PoblacionInter)):
-  print("   C="+str(CalidadP[Poblacion.index(PoblacionInter[i])])+', '+str(PoblacionInter[i]))
+            Padres = random.sample(PoblacionInter, 2)
+            hijo1, hijo2 = Cruce(Padres[0], Padres[1])
+            hijo1M = Mutacion(hijo1, 6)
+            hijo2M = Mutacion(hijo2, 6)
+            Poblacion.append(hijo1M)
+            Poblacion.append(hijo2M)
+
+        # guardar la poblacion  en un archivo csv usando
+        with open('poblacion.txt', 'w') as file:
+            for i in range(len(Poblacion)):
+                for j in range(len(Poblacion[i])):
+                    file.write(str(Poblacion[i][j]))
+                    if j < len(Poblacion[i])-1:
+                        file.write(",")
+                if i < len(Poblacion)-1:
+                    file.write("\n")
+
+    print("==============RESPUESTA===============")
+    solucion = Poblacion[list(CalidadP).index(min(CalidadP))]
+
+    print("Número de iteraciones: "+str(k))
+    print("Distancia mas corta: "+str(min(CalidadP)))
+    # print("Solución: "+str(solucion))
+    # print("Última poblacion intermedia: ")
+    # for i in range(len(PoblacionInter)):
+    #   print("   C="+str(CalidadP[Poblacion.index(PoblacionInter[i])])+', '+str(PoblacionInter[i]))
--- TP1/Funciones_Genetico.py
+++ TP1/Funciones_Genetico.py
@@ -5,82 +5,86 @@ from Temple2 import temple_simulado
 import time
 
 
-def DeProductosAEstantes(orden,IdEstantes,configuracion):
-  orden_E=[]
-  for producto in orden:
-    orden_E.append(IdEstantes[configuracion.index(producto)])
-  return orden_E
+def DeProductosAEstantes(orden, IdEstantes, configuracion):
+    orden_E = []
+    for producto in orden:
+        orden_E.append(IdEstantes[configuracion.index(producto)])
+    return orden_E
 
-    
+
 def Calidad(Poblacion_P, Ordenes_P, IdEstantes):
+    Calidad = []
+    for configuracion in Poblacion_P:
+        Calidad.append(Calidad_individual(configuracion, Ordenes_P, IdEstantes))
+    return list(Calidad)
 
-  Calidad = []
-  for configuracion in Poblacion_P:
-    t0 = time.time()
+
+def Calidad_individual(Individuo, Ordenes_P, IdEstantes):
+    # t0 = time.time()
     suma = 0
     divisor = len(Ordenes_P)
     
     for orden in Ordenes_P:
         # Seria util que el temple simulado se ejecute de forma concurrente pero son muchas ordenes.
         # Convierto la orden a una lista de estantes en donde estan esos productos.
-        orden_E = DeProductosAEstantes(orden, IdEstantes, configuracion)
+        orden_E = DeProductosAEstantes(orden, IdEstantes, Individuo)
         suma += temple_simulado(orden_E)
     # La calidad del individuo es el promedio de lo que se demora en cumplir cada orden
     calidadOrden = suma / divisor
-        
-    Calidad.append(calidadOrden)
-    print("\tcalidad individuo: "+str(calidadOrden)+" tiempo calculo: "+str(time.time()-t0))
-  return list(Calidad)
-
+    
+    # print("\tcalidad individuo: "+str(calidadOrden)
+    #        # +" tiempo calculo: "+str(time.time()-t0)
+    #       )
+    return calidadOrden
 
 def Cruce(padre1, padre2):
     hijo1 = []
     hijo2 = []
-    #cantidad de elementos igual a "vac" en padre1
+    # cantidad de elementos igual a "vac" en padre1
     N_vac = padre1.count('vacio')
 
     punto_cruce = random.randint(1, len(padre1)-1)
-    
+
     hijo1.extend(padre1[:punto_cruce])
     for gen in padre2:
-      if (gen not in list(hijo1)) or (gen=='vacio' and hijo1.count('vacio')<N_vac):
-        hijo1.append(gen)
+        if (gen not in list(hijo1)) or (gen == 'vacio' and hijo1.count('vacio') < N_vac):
+            hijo1.append(gen)
 
     hijo2.extend(padre2[:punto_cruce])
     for gen in padre1:
-      if (gen not in list(hijo2)) or (gen=='vacio' and hijo2.count('vacio')<N_vac):
-        hijo2.append(gen)
+        if (gen not in list(hijo2)) or (gen == 'vacio' and hijo2.count('vacio') < N_vac):
+            hijo2.append(gen)
 
-    #if len(hijo1) != len(padre1):
+    # if len(hijo1) != len(padre1):
     #  print(hijo1.count('vacio'))
     #  print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     #  pausa=input("pausa")
-    
+
     return hijo1, hijo2
 
-def Mutacion(Individuo,cantidad):
-  Copia=Individuo.copy()
+
+def Mutacion(Individuo, cantidad):
+    Copia = Individuo.copy()
+
+    for i in range(cantidad):
+        # selecionar 2 puntos al azar
+        punto1 = np.random.randint(0, len(Copia))
+        punto2 = np.random.randint(0, len(Copia))
+        # intercambiar
+        Copia[punto1], Copia[punto2] = Copia[punto2], Copia[punto1]
+
+    return list(Copia)
 
 
-  for i in range(cantidad):
-    #selecionar 2 puntos al azar
-    punto1=np.random.randint(0,len(Copia))
-    punto2=np.random.randint(0,len(Copia))
-    #intercambiar
-    Copia[punto1],Copia[punto2]=Copia[punto2],Copia[punto1]
+def Convergencia(calidades, N):
+    # ordenar las calidades de menor a mayor
+    calidades = list(calidades)
+    # contar la canticade de elementos que son iguales
+    Iguales = calidades.count(calidades[0])
+    return Iguales >= N
 
 
-  return list(Copia)
+def SelecionarPoblacion(Poblacion, calidad, Cantidad):
+    CalidadOrd, PoblacionOrdenada = list(sort_together([calidad, Poblacion]))
 
-def Convergencia(calidades,N):
-  #ordenar las calidades de menor a mayor
-  calidades=list(calidades)
-  #contar la canticade de elementos que son iguales
-  Iguales=calidades.count(calidades[0])
-  return Iguales>=N
-
-def SelecionarPoblacion(Poblacion,calidad,Cantidad):
-  CalidadOrd,PoblacionOrdenada = list(sort_together([calidad, Poblacion]))
-
-  
-  return(list(CalidadOrd[:Cantidad]),list(PoblacionOrdenada[:Cantidad]))
+    return(list(CalidadOrd[:Cantidad]), list(PoblacionOrdenada[:Cantidad]))
