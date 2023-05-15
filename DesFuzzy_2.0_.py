54c042abd0ff559ad4cea711c1c031cd67445a7d
Maximiliano Sotar <89359193+Maximiliano-Sotar@users.noreply.github.com>
2023-05-15 10:34:28 -0300

    Delete DesFuzzy_2.0_.py

--- DesFuzzy_2.0_.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-def Desfuzzyficacion(Dominio, fuzzy):
-    # calcula el valor nitido por media de centros
-    L0 = (Dominio[1]-Dominio[0])/4
-    centros = np.linspace(Dominio[0] + L0, Dominio[1] - L0, 5)
-    valor_nitido = np.sum(fuzzy * centros) / np.sum(fuzzy)
-    return valor_nitido
